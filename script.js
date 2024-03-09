f4d1da9e5b16e057024622711c5950c225c8ce4c
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2024-03-09 20:23:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+
+//import * as XLSX from 'xlsx';
+// const XLSX = window.XLSX;
+// async function processFile(file) {
+//   const jsonData = await readExcel(file);
+//   console.log(jsonData);
+// }
+// const file = new File([""], "example.xlsx", { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+// processFile(file);
+// function processFile(file) {
+//   const jsonData = readExcel(file);
+//   jsonData.then((data) => {
+//     console.log(data);
+//   });
+// }
+// const file = new File([""], "data.xlsx", { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+// processFile(file);
+
+async function processFile() {
+  const response = await fetch('data.xlsx');
+  const data = await response.arrayBuffer();
+  const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+  const file = new File([blob], 'data.xlsx', { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+  const jsonData = await readExcel(file);
+  console.log(jsonData);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     // Function to read and display Excel content
     function readExcel() {
       // Replace 'your-excel-file.xlsx' with your actual Excel file name
-      const excelFile = 'product.xlsx';
+      const excelFile = 'data.xlsx';
       
       // Read Excel file
       const workbook = XLSX.readFile(excelFile);
